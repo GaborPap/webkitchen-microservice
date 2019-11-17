8122c17cf1c55d728919484802b8513c17a16a44
GaborPap <ppgabor@gmail.com>
2019-11-17 19:52:19 +0100

    Added webkitchen service.

---
+++ webkitchen-service/src/main/java/com/codecool/webkitchen/WebkitchenApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.webkitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebkitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebkitchenApplication.class, args);
+	}
+
+}
---
+++ webkitchen-service/src/test/java/com/codecool/webkitchen/WebkitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.webkitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebkitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
