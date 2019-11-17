da8fc73920d666cc9aea011fdc0d107c9f5b93b7
GaborPap <ppgabor@gmail.com>
2019-11-17 12:23:07 +0100

    Implemented model for recipes.

---
+++ recipe-service/src/main/java/com/codecool/recipe/model/Recipe.java
@@ -0,0 +1,35 @@
+package com.codecool.recipe.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+@Component
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Recipe {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Map<Integer, Integer> ingredients;
+
+    @NotNull
+    private String name;
+    private String description;
+    private String image;
+    private Integer servings; //adagok száma
+    private String directions; //elkészítés
+}
