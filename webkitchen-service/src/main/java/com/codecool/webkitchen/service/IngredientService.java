12db5993d9358d284f69d606dbcc371f699d526e
GaborPap <ppgabor@gmail.com>
2019-11-18 02:14:47 +0100

    Implemented service for ingredients.

---
+++ webkitchen-service/src/main/java/com/codecool/webkitchen/service/IngredientService.java
@@ -0,0 +1,34 @@
+package com.codecool.webkitchen.service;
+
+import com.codecool.ingredient.model.Ingredient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class IngredientService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${ingredients.url}")
+    private String ingredientUrl;
+
+    public List<Ingredient> getIngredients() {
+        ResponseEntity<List<Ingredient>> response = restTemplate.exchange(
+                ingredientUrl + "/",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Ingredient>>() {
+                }
+        );
+
+        return response.getBody();
+    }
+}
