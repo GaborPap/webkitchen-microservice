708eda77c3114e1907fb11856db6b72af8e40cc4
GaborPap <ppgabor@gmail.com>
2019-11-18 11:02:43 +0100

    Configured zuul to access webkitchen service only.

--- zuul-gateway/src/main/java/com/codecool/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/codecool/zuulgateway/ZuulGatewayApplication.java
@@ -1,13 +1,42 @@
 package com.codecool.zuulgateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
+@EnableZuulProxy
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 }
