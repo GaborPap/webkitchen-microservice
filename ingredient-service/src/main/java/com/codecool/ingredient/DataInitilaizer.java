e6d4e9977bf041607d188b387a71ff6f3cce77b1
GaborPap <ppgabor@gmail.com>
2019-11-19 01:26:06 +0100

    Added item to ingredient data initializer

--- ingredient-service/src/main/java/com/codecool/ingredient/DataInitilaizer.java
+++ ingredient-service/src/main/java/com/codecool/ingredient/DataInitilaizer.java
@@ -16,16 +16,31 @@ public class DataInitilaizer implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Ingredient salt = Ingredient.builder()
-                .name("salt")
+                .name("Salt")
+                .description("Salt is a mineral composed primarily of sodium chloride (NaCl), a chemical compound belonging to the larger class of salts; salt in its natural form as a crystalline mineral is known as rock salt or halite.")
+                .amount(4)
+                .ImageUrl("salt.jpg")
                 .build();
 
         Ingredient sugar = Ingredient.builder()
-                .name("sugar")
+                .name("Sugar")
+                .description("Sugar is the generic name for sweet-tasting, soluble carbohydrates, many of which are used in food. Simple sugars, also called monosaccharides, include glucose, fructose, and galactose.")
+                .amount(1)
+                .ImageUrl("sugar.jpg")
+                .build();
+
+        Ingredient pepper = Ingredient.builder()
+                .name("Pepper")
+                .description("The chili pepper (also chile, chile pepper, chilli pepper, or chilli[4]), from Nahuatl chīlli (Nahuatl pronunciation: [ˈt͡ʃiːli] (About this soundlisten)), is the fruit of plants from the genus Capsicum which are members of the nightshade family,.")
+                .amount(2)
+                .ImageUrl("pepper.jpg")
                 .build();
 
 
         ingredientRepository.save(salt);
         ingredientRepository.save(sugar);
+        ingredientRepository.save(pepper);
+
 
 
     }
